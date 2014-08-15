3b2290db019383abfc47823b93d26166ba1bbbe1
ragnarok56 <kevin.nacios@gmail.com>
2014-08-14 23:00:07 -0400

    reorganize some stuff, clean up react components, experiment with rxjs and realize i dont completely understand how it works yet :\

--- build/test.js
+++ build/test.js
@@ -1,31 +1,6 @@
 /** @jsx React.DOM */
 ; (function ($, Rx, React, undefined) {
-    var uri = "wss://qa.sockets.stackexchange.com";
-    var defaultStream = "155-questions-active";
-
-    var ws = new WebSocket(uri);
-
-    var status = document.getElementById("socketStatus");
-
-    ws.onopen = function () { 
-        writeStatus("web socket connection opened");
-
-        ws.send(defaultStream);
-    }; 
-
-    ws.onclose = function () {
-        writeStatus("web socket connection closed");
-    }
-
-    ws.onerror = function () {
-        writeStatus("web socket connection error");
-    }
-
-    function writeStatus(s) {
-        status.innerHTML = "<p>" + s + "</p>";
-    }
-
-
+    
     /*
     siteBaseHostAddress: stackoverflow.com
 id: 25105700
@@ -40,74 +15,180 @@ apiSiteParameter: stackoverflow
     */
 
     // react stuff
+    //var Tag = React.createClass({
+    //    render: function () {
+    //        return <a href="#"
+    //    }
+    //});
+
     var Tags = React.createClass({displayName: 'Tags',
         render: function () {
-
             return React.DOM.div(null, this.props.items.map(function (x) { return '[' + x + ']'; }).join(' '));
         }
     });
 
-    var Question = React.createClass({displayName: 'Question',
+    var Update = React.createClass({displayName: 'Update',
         render: function () {
-            var item = JSON.parse(this.props.item);
-
             var parseDate = function (seconds) {
                 var d = new Date(1970, 0, 1);
                 d.setSeconds(seconds);
                 return d;
             };
 
-            return React.DOM.div({class: "question", key: item.id}, 
+            var item = this.props.item;
+
+            return (
+                React.DOM.div({className: "update"}, 
                     React.DOM.a({href: item.url}, unescape(item.titleEncodedFancy)), 
                     React.DOM.span({style: {margin: "0 0 0 5px"}}, React.DOM.b(null, "[", item.apiSiteParameter, "]")), 
                     React.DOM.div(null, React.DOM.small(null, parseDate(item.lastActivityDate).toISOString())), 
                     Tags({items: item.tags})
-                );
+                )
+            );
                 
         }
     });
 
-    var Questions = React.createClass({displayName: 'Questions',
-        getInitialState: function () {
-            return { questions: [] };
-        },
-        handleClear: function () {
-            this.setState(this.getInitialState());
-        },
-        componentDidMount: function() {
-            var component = this;
-
-            this.props.source.onmessage = function (x) {
-                var d = JSON.parse(x.data)
-
-                var state = { 
-                    questions: [d.data].concat(component.state.questions)
-                };
-
-                component.setState(state);
-            }
-        },
+    var Updates = React.createClass({displayName: 'Updates',
         render: function () {
-            var createQuestion = function (data) {
-                return Question({item: data});
+            var createUpdate = function (data) {
+                return React.DOM.li({key: data.id}, Update({item: data}));
             };
 
-            return React.DOM.div({class: "items"}, 
-                    React.DOM.button({class: "clear", onClick: this.handleClear}, "clear"), 
-                    React.DOM.h3(null, "Questions (latest updates)"), 
-                    React.DOM.ul(null, this.state.questions.map(createQuestion))
-                );
+            return (
+                React.DOM.ul({className: "list-unstyled"}, this.props.updates.map(createUpdate))
+            );
         }
     })
+
+    var Filter = React.createClass({displayName: 'Filter',
+        render: function () {
+            return (
+                React.DOM.form({onSubmit: this.props.onFilter}, 
+                    React.DOM.input({type: "text", placeholder: "Tags", value: this.props.filterTags})
+                )
+            );
+        }
+    });
 
     var App = React.createClass({displayName: 'App',
         render: function () {
-            return React.DOM.div({style: { margin: "10px"}}, 
-                    Questions({source: this.props.source})
+            return (
+                React.DOM.div({style: { margin: 10}}, 
+                    React.DOM.button({type: "button", className: "btn btn-primary", onClick: this.props.clear}, "Clear"), 
+                    Filter({onFilter: this.props.filter}), 
+                    React.DOM.h3(null, "Latest Updates"), 
+                    Updates({updates: this.props.data})
                 )
-        }
-    })
+            );
+        }   
+    });
 
-    React.renderComponent(App({source: ws}), document.getElementById('mount'));
+    var updates = [];
+
+    function renderUI(data) {
+        React.renderComponent(App({data: data, clear: clearUpdates, filter: filterUpdates}), document.getElementById('main'));
+    }
+
+    function filterUpdates() {
+
+    }
+
+    function clearUpdates() {
+        updates = [];
+
+        renderUI(updates);
+    }
+
+    // rx stuff
+    
+
+    // web socket stuff
+    function fromWebSocket(address, onOpen) {
+        var ws = new WebSocket(address);
+
+        // Handle the data
+        var observable = Rx.Observable.create(function (obs) {
+            // Handle messages  
+            ws.onmessage = obs.onNext.bind(obs);
+            ws.onerror = obs.onError.bind(obs);
+            ws.onclose = obs.onCompleted.bind(obs);
+
+            // Return way to unsubscribe
+            return ws.close.bind(ws);
+        });
+
+        var observer = Rx.Observer.create(function (data) {
+            if (ws.readyState === WebSocket.OPEN) { 
+                ws.send(data); 
+            }
+        });
+
+        ws.onopen = onOpen;
+
+        return Rx.Subject.create(observer, observable);
+    }
+
+    var uri = "wss://qa.sockets.stackexchange.com";
+    var defaultStream = "155-questions-active";
+    var status = document.getElementById("socketStatus");
+
+    function writeStatus(s) {
+        status.innerHTML = "<p>" + s + "</p>";
+    }
+
+    
+
+    var wsSubject = fromWebSocket(uri, function () {
+        writeStatus("web socket connection opened");
+
+        wsSubject.onNext(defaultStream);
+    });
+
+    wsSubject.subscribe(function(x) {
+        console.log("Received Message: " + x.data);
+
+        var update = JSON.parse(JSON.parse(x.data).data);
+
+        updates = [update].concat(updates);
+
+        renderUI(updates);
+    });
+
+
+    // this rx stuff doesnt work really :\
+
+    //var observableUpdates = wsSubject
+    //    .map(function (x) {
+    //        console.log("Received Message");
+    //        return JSON.parse(x.data);
+    //    })
+    //    .where(function (x) {
+    //        return true;
+    //    })
+    //    .reduce(function (previous, current) {
+    //        var update = JSON.parse(current.data);
+
+    //        return [update].concat(previous);
+    //    }, updates);
+
+    
+    //observableUpdates.subscribe(
+    //    function (data) {
+    //        console.log(data);
+    //        updates = data;
+    //        //updates = [d.data].concat(updates).map(JSON.parse);
+
+    //        renderUI(updates);
+    //    }, 
+    //    function (error) {
+    //        writeStatus("web socket connection error: " + error);
+    //    },
+    //    function () {
+    //        writeStatus("web socket connection closed");
+    //    }
+    //);
+
+    renderUI(updates);
 
 }.call(this, jQuery, Rx, React));
--- src/test.js
+++ src/test.js
@@ -1,31 +1,6 @@
 /** @jsx React.DOM */
 ; (function ($, Rx, React, undefined) {
-    var uri = "wss://qa.sockets.stackexchange.com";
-    var defaultStream = "155-questions-active";
-
-    var ws = new WebSocket(uri);
-
-    var status = document.getElementById("socketStatus");
-
-    ws.onopen = function () { 
-        writeStatus("web socket connection opened");
-
-        ws.send(defaultStream);
-    }; 
-
-    ws.onclose = function () {
-        writeStatus("web socket connection closed");
-    }
-
-    ws.onerror = function () {
-        writeStatus("web socket connection error");
-    }
-
-    function writeStatus(s) {
-        status.innerHTML = "<p>" + s + "</p>";
-    }
-
-
+    
     /*
     siteBaseHostAddress: stackoverflow.com
 id: 25105700
@@ -40,74 +15,180 @@ apiSiteParameter: stackoverflow
     */
 
     // react stuff
+    //var Tag = React.createClass({
+    //    render: function () {
+    //        return <a href="#"
+    //    }
+    //});
+
     var Tags = React.createClass({
         render: function () {
-
             return <div>{this.props.items.map(function (x) { return '[' + x + ']'; }).join(' ')}</div>;
         }
     });
 
-    var Question = React.createClass({
+    var Update = React.createClass({
         render: function () {
-            var item = JSON.parse(this.props.item);
-
             var parseDate = function (seconds) {
                 var d = new Date(1970, 0, 1);
                 d.setSeconds(seconds);
                 return d;
             };
 
-            return <div class="question" key={item.id}>
+            var item = this.props.item;
+
+            return (
+                <div className="update">
                     <a href={item.url}>{unescape(item.titleEncodedFancy)}</a>
                     <span style={{margin: "0 0 0 5px"}}><b>[{item.apiSiteParameter}]</b></span>
                     <div><small>{parseDate(item.lastActivityDate).toISOString()}</small></div>
                     <Tags items={item.tags}/>
-                </div>;
+                </div>
+            );
                 
         }
     });
 
-    var Questions = React.createClass({
-        getInitialState: function () {
-            return { questions: [] };
-        },
-        handleClear: function () {
-            this.setState(this.getInitialState());
-        },
-        componentDidMount: function() {
-            var component = this;
-
-            this.props.source.onmessage = function (x) {
-                var d = JSON.parse(x.data)
-
-                var state = { 
-                    questions: [d.data].concat(component.state.questions)
-                };
-
-                component.setState(state);
-            }
-        },
+    var Updates = React.createClass({
         render: function () {
-            var createQuestion = function (data) {
-                return <Question item={data}/>;
+            var createUpdate = function (data) {
+                return <li key={data.id}><Update item={data}/></li>;
             };
 
-            return <div class="items">
-                    <button class="clear" onClick={this.handleClear}>clear</button>
-                    <h3>Questions (latest updates)</h3>
-                    <ul>{this.state.questions.map(createQuestion)}</ul>
-                </div>;
+            return (
+                <ul className="list-unstyled">{this.props.updates.map(createUpdate)}</ul>
+            );
         }
     })
+
+    var Filter = React.createClass({
+        render: function () {
+            return (
+                <form onSubmit={this.props.onFilter}>
+                    <input type="text" placeholder="Tags" value={this.props.filterTags}/>
+                </form>
+            );
+        }
+    });
 
     var App = React.createClass({
         render: function () {
-            return <div style={{ margin: "10px" }} > 
-                    <Questions source={this.props.source}/>
+            return (
+                <div style={{ margin: 10 }}>
+                    <button type="button" className="btn btn-primary" onClick={this.props.clear}>Clear</button>
+                    <Filter onFilter={this.props.filter}/>
+                    <h3>Latest Updates</h3>
+                    <Updates updates={this.props.data}/>
                 </div>
-        }
-    })
+            );
+        }   
+    });
 
-    React.renderComponent(<App source={ws}/>, document.getElementById('mount'));
+    var updates = [];
+
+    function renderUI(data) {
+        React.renderComponent(<App data={data} clear={clearUpdates} filter={filterUpdates}/>, document.getElementById('main'));
+    }
+
+    function filterUpdates() {
+
+    }
+
+    function clearUpdates() {
+        updates = [];
+
+        renderUI(updates);
+    }
+
+    // rx stuff
+    
+
+    // web socket stuff
+    function fromWebSocket(address, onOpen) {
+        var ws = new WebSocket(address);
+
+        // Handle the data
+        var observable = Rx.Observable.create(function (obs) {
+            // Handle messages  
+            ws.onmessage = obs.onNext.bind(obs);
+            ws.onerror = obs.onError.bind(obs);
+            ws.onclose = obs.onCompleted.bind(obs);
+
+            // Return way to unsubscribe
+            return ws.close.bind(ws);
+        });
+
+        var observer = Rx.Observer.create(function (data) {
+            if (ws.readyState === WebSocket.OPEN) { 
+                ws.send(data); 
+            }
+        });
+
+        ws.onopen = onOpen;
+
+        return Rx.Subject.create(observer, observable);
+    }
+
+    var uri = "wss://qa.sockets.stackexchange.com";
+    var defaultStream = "155-questions-active";
+    var status = document.getElementById("socketStatus");
+
+    function writeStatus(s) {
+        status.innerHTML = "<p>" + s + "</p>";
+    }
+
+    
+
+    var wsSubject = fromWebSocket(uri, function () {
+        writeStatus("web socket connection opened");
+
+        wsSubject.onNext(defaultStream);
+    });
+
+    wsSubject.subscribe(function(x) {
+        console.log("Received Message: " + x.data);
+
+        var update = JSON.parse(JSON.parse(x.data).data);
+
+        updates = [update].concat(updates);
+
+        renderUI(updates);
+    });
+
+
+    // this rx stuff doesnt work really :\
+
+    //var observableUpdates = wsSubject
+    //    .map(function (x) {
+    //        console.log("Received Message");
+    //        return JSON.parse(x.data);
+    //    })
+    //    .where(function (x) {
+    //        return true;
+    //    })
+    //    .reduce(function (previous, current) {
+    //        var update = JSON.parse(current.data);
+
+    //        return [update].concat(previous);
+    //    }, updates);
+
+    
+    //observableUpdates.subscribe(
+    //    function (data) {
+    //        console.log(data);
+    //        updates = data;
+    //        //updates = [d.data].concat(updates).map(JSON.parse);
+
+    //        renderUI(updates);
+    //    }, 
+    //    function (error) {
+    //        writeStatus("web socket connection error: " + error);
+    //    },
+    //    function () {
+    //        writeStatus("web socket connection closed");
+    //    }
+    //);
+
+    renderUI(updates);
 
 }.call(this, jQuery, Rx, React));
